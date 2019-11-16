6471163e2062e5ed4fdbc83446505ed9033848dd
Piero Canepa <pcanepa@nus.edu.sg>
2019-11-17 05:20:56 +0800

    update mof image

--- update.sh
+++ update.sh
@@ -1,7 +1,12 @@
 #!/bin/bash 
 
+
+read -p "Enter text for commit: " tcommit
+
 cd ../webpage_CaRe/
 bundle exec jekyll build 
 cd ../caneparesearch.github.io
 cp -r  ../webpage_CaRe/_site/* .
 git add *
+git commit -m "$tcommit"
+git push 
